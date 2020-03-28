f9c32a84758a64697acbb493c2fa8bf2dcc162c6
unknown <rayanbensaad@hotmail.fr>
2020-03-28 18:17:07 +0100

    integration template user

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,49 @@
 
 import React, { Component } from 'react';
-import { Link, Route, Switch, BrowserRouter } from 'react-router-dom';
+import {BrowserRouter as Router, HashRouter, Route, Switch} from 'react-router-dom';
+
 
 import Login from "./components/Login";
 import Register from "./components/Register";
 import DashBoard from './dashboard/Dashboard';
 import withAuth from './tools/withAuth';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div>
- {/*       <ul>
-          <li><Link to="/dashboard"></Link></li>
-           <li><Link to="/register">Register</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/home">Home</Link></li>
-        </ul>
- */}
-    <BrowserRouter>
-        <Switch>
-        <Route path="/dashboard" component={DashBoard} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-        </BrowserRouter>
+       
+
+        <Router>
+        <Navbar />
+        <div>
+        
+          <switch>
+            <Route path="/dashboard" component={DashBoard} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+             
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/services' component={Services} />
+            <Route path='/portfolio' component={Portfolio} />
+            <Route path='/blog' component={Blog} />
+            <Route Path='/contact' component={Contact} />
+          </switch>
+          </div>
+        </Router>
       </div>
     );
   }
 }
+
 export default App;
 
 
