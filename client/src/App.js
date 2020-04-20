7f9341b92314afd0fd9a0273da166bf1859d9961
mouheb <mouheb.bouazra@esprit.tn>
2020-04-20 21:23:32 +0100

    fix switch

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import DashBoard from './dashboard/Dashboard';
 import Home from './template/home';
 import withAuth from './tools/withAuth';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
@@ -24,30 +23,13 @@ class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
- {/*       <ul>
-          <li><Link to="/dashboard"></Link></li>
-           <li><Link to="/register">Register</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/home">Home</Link></li>
-        </ul>
- */}
-    <BrowserRouter>
-        <Switch>
-        <Route path="/dashboard" component={DashBoard} />
-        <Route exact path="/" component={Home} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-        </BrowserRouter>
-=======
        
 
         <Router>
         <Navbar />
         <div>
         
-          <switch>
+          <Switch>
             <Route path="/dashboard" component={DashBoard} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
@@ -61,10 +43,9 @@ class App extends Component {
             <Route path='/portfolio' component={Portfolio} />
             <Route path='/blog' component={Blog} />
             <Route Path='/contact' component={Contact} />
-          </switch>
+          </Switch>
           </div>
         </Router>
->>>>>>> ea875983ae7319a0fd581c033210faad152f3ea4
       </div>
     );
   }
